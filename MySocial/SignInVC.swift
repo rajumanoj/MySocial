f13d05cd33538813a9d8bc686f102e732bb0b286
rajumanoj <Manoj@manoj.attlocal.net>
2017-05-09 16:00:30 -0700

    First Commit

---
+++ MySocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySocial
+//
+//  Created by Manoj Gadhiraju on 5/9/17.
+//  Copyright © 2017 Manoj. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MySocialTests/Pods-MySocialTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MySocialTests : NSObject
+@end
+@implementation PodsDummy_Pods_MySocialTests
+@end
