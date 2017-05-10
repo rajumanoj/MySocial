ecc8c11ab23ef57eb9e3e37d5bb874f6af8f5d6e
rajumanoj <Manoj@manoj.attlocal.net>
2017-05-09 19:17:30 -0700

    Added custom classes to take care of views

---
+++ MySocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MySocial
+//
+//  Created by Manoj Gadhiraju on 5/9/17.
+//  Copyright © 2017 Manoj. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
